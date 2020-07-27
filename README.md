7daa80663d1909e43c7afb8e4e588e27751695ac
illialaz <illia.laziuk2@gmail.com>
2020-07-27 16:47:10 +0300

    Business-hours of shop
    - Interact with airtable-cli

---
+++ README.md
@@ -0,0 +1,9 @@
+# Start your app
+
+### Create Airtable base at https://airtable.com/   I called my base Shop, I named the table Business Hours and I made 2 columns: Day and Hours, adding the days of the week and for each day the opening hours
+
+### Find your AIRTABLE_API_KEY
+
+### npm install 
+
+## type AIRTABLE_API_KEY=<yourkey> nodemon app.js in terminal
---
+++ app.js
@@ -0,0 +1,36 @@
+const base = require('airtable').base('app8Qp2nOEQoNO3lj')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, '.'))
+
+let records
+
+app.get('/', async (req, res) => {
+  if (records) {
+    console.log('cached')
+    res.render('page', {
+      records,
+    })
+  } else {
+    (async () => {
+      records = await base('Business Hours')
+        .select({
+          view: 'Grid view',
+        })
+        .firstPage()
+
+      res.render('page', {
+        records,
+      })
+
+      setTimeout(() => {
+        records = null
+      }, 10 * 1000)
+    })()  
+  }
+})
+
+app.listen(3000, () => console.log('Server is ready'))
